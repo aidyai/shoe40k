3fb92ead3ccf605b5ef9974715d8a0cccc5ec381
aidyosu <aidyosu553@gmail.com>
2024-03-26 08:23:39 -0700

    Create dataset.py

---
+++ src/dataset.py
@@ -0,0 +1,23 @@
+import torch
+
+class IMDbDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+train_dataset = IMDbDataset(train_encodings, train_labels)
+val_dataset = IMDbDataset(val_encodings, val_labels)
+#test_dataset = IMDbDataset(test_encodings, test_labels)
+
+
+
+train_dataset.set_transform(preprocess_train)
+val_dataset.set_transform(preprocess_val)
