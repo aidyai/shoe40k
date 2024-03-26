1f985c48f3975bff76b24eee2bc9b2d119923620
aidyosu <aidyosu553@gmail.com>
2024-03-26 08:37:19 -0700

    Update transforms.py

--- src/transforms.py
+++ src/transforms.py
@@ -1,3 +1,31 @@
+from transformers import ViTImageProcessor
+from torchvision.transforms import v2
+
+processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224', do_rescale = False, return_tensors = 'pt')
+
+image_mean, image_std = processor.image_mean, processor.image_std
+size = processor.size["height"]
+
+normalize = v2.Normalize(mean=image_mean, std=image_std)
+
+train_transform = v2.Compose([
+      v2.Resize((processor.size["height"], processor.size["width"])),
+      v2.RandomHorizontalFlip(0.4),
+      v2.RandomVerticalFlip(0.1),
+      v2.RandomApply(transforms=[v2.RandomRotation(degrees=(0, 90))], p=0.5),
+      v2.RandomApply(transforms=[v2.ColorJitter(brightness=.3, hue=.1)], p=0.3),
+      v2.RandomApply(transforms=[v2.GaussianBlur(kernel_size=(5, 9))], p=0.3),
+      v2.ToTensor(),
+      normalize
+      #transforms.Normalize(mean = (0.485, 0.456, 0.406), std = (0.229, 0.224, 0.225))
+ ])
+
+test_transform = v2.Compose([
+    v2.Resize((processor.size["height"], processor.size["width"])),
+    v2.ToTensor(),
+    normalize
+])
+
 from torchvision.transforms import (
     CenterCrop,
     Compose,
