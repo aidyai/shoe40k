824742ffbaf677cb2f38fec804190745b4735fe2
aidyosu <aidyosu553@gmail.com>
2024-03-27 21:29:05 -0700

    Update train.py

--- train.py
+++ train.py
@@ -1,33 +1,73 @@
-from transformers import TrainingArguments, Trainer
+import torch
+import wandb
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
 
-training_args=TrainingArguments(
-    output_dir=os.getenv("WANDB_NAME"),
-    remove_unused_columns=False,
-    evaluation_strategy="epoch",
-    save_strategy="epoch",
-    learning_rate=5e-3,
-    per_device_train_batch_size=32,
-    per_device_eval_batch_size=32,
-    gradient_accumulation_steps=4,
-    fp16=True,
-    num_train_epochs=2,
-    logging_steps=10,
-    load_best_model_at_end=True,
-    metric_for_best_model="accuracy",
-    push_to_hub=False,
-    label_names=["labels"],
-    report_to="wandb",
-    run_name=os.getenv("WANDB_NAME"),
-)
 
-trainer=Trainer(
-    peft_model,
-    training_args,
-    train_dataset=train_dataset,         # training dataset
-    eval_dataset=val_dataset,             # evaluation dataset
-    tokenizer=image_processor,
-    compute_metrics=compute_metrics,
-    data_collator=collate_fn,
-)
+from dataset import Shoe40kDataModule
+from model import Shoe40kClassificationModel
+from image_logger import ImagePredictionLogger
 
-train_results=trainer.train()
+
+
+
+
+def train(batch_size: int, 
+          epochs: int,
+          csv_path: str,
+          dataset_path: str,
+          ):
+    
+    """Trains a PyTorch Lightning model using Weights and Biases"""
+
+    # Instantiate the model
+    model = Shoe40kClassificationModel()
+
+    # Instantiate the LightningDataModule
+    data_module = Shoe40kDataModule(csv_path, dataset_path, batch_size=batch_size)
+
+    # Load the DataLoaders for both the train and validation datasets
+    train_loader = data_module.train_dataloader()
+    val_loader = data_module.val_dataloader()
+
+    # Samples required by the custom ImagePredictionLogger callback to log image predictions.
+    val_samples = next(iter(val_loader))
+
+    # Create the configuration of the current run
+    wandb_config = {
+        'batch_size': batch_size,
+        'epochs': epochs,
+        'train_batches': len(train_loader),
+        'val_batches': len(val_loader),
+        'dataset': 'shoe40k',
+        'dataset_train_size': len(data_module.train_dataloader()),
+        'dataset_val_size': len(data_module.val_dataloader()),
+        'input_shape': '[3,344,344]',
+        'channels_last': False,
+        'criterion': 'CrossEntropyLoss',
+        'optimizer': 'Adam'
+    }
+
+    # Init the PyTorch Lightning WandbLogger (you need to `wandb login` first!)
+    wandb_logger = WandbLogger(project='shoe40k', job_type='train', config=wandb_config)
+
+    # Initialize Callbacks
+    early_stop_callback = pl.callbacks.EarlyStopping(monitor="val_loss")
+    checkpoint_callback = pl.callbacks.ModelCheckpoint()
+
+    # Initialize a trainer
+    trainer = pl.Trainer(max_epochs=50,
+                        progress_bar_refresh_rate=20, 
+                        gpus=1, 
+                        logger=wandb_logger,
+                        callbacks=[early_stop_callback,
+                                    ImagePredictionLogger(val_samples)],
+                        checkpoint_callback=checkpoint_callback)
+
+
+
+    trainer.fit(model, train_loader, val_loader)
+
+    # Close wandb run
+    wandb.finish()
+
