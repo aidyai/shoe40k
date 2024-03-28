295c5e6989281e69dd9d33399a015451c1996b0f
aidyosu <aidyosu553@gmail.com>
2024-03-27 19:33:50 -0700

    Update and rename metrics.py to image_logger.py

--- src/metrics.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import evaluate
-
-metric=evaluate.load("accuracy")
-
-def compute_metrics(eval_pred):
-    predictions=np.argmax(eval_pred.predictions, axis=1)
-    return metric.compute(predictions=predictions, references=eval_pred.label_ids)
