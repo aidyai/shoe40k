daca296c86fe2888c1e55881f493999810dfd912
aidyosu <aidyosu553@gmail.com>
2024-03-29 16:27:33 -0700

    Update train.py

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ if __name__ == '__main__':
     # Define variables
     batch_size = 32
     epochs = 10
-    csv_path = ''
-    dataset_path = ''
+    csv_path = '/content/FOOT40K.csv'
+    dataset_path = '/content/FOOT40k'
 
     # Call the train function
     train(batch_size, epochs, csv_path, dataset_path)
